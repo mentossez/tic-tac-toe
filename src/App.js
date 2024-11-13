55c7dedde7409cdbbf8f06fdd5652b46b8ca3cfc
mentossez <akavathekar03@gmail.com>
2024-11-14 00:55:31 +0530

    added history logic and reset

--- src/App.js
+++ src/App.js
@@ -13,10 +13,14 @@ function Square({ value, onClick, winnerCombination, currentIndex }) {
   );
 }
 
+// Need to write next back login for isXNext
+
 export default function App() {
   const emptyArray = Array(9).fill(null);
   const [values, setValue] = useState(emptyArray);
-  const [IsXNext, setIsXNext] = useState(true);
+  const [IsXNext, setIsXNext] = useState(false);
+  const [history, setHistory] = useState([emptyArray]);
+  const [currentStep, setCurrentStep] = useState([]);
 
   function handleSquareClick(i) {
     if ((values[i]) || calculateWinner(values)) {
@@ -24,12 +28,32 @@ export default function App() {
     }
     const newArray = values.slice();
     if (IsXNext) {
-      newArray[i] = "X";
+      newArray[i] = 'X';
     } else {
-      newArray[i] = "O";
+      newArray[i] = 'O';
     }
     setValue(newArray);
+    setHistory([...history, newArray]);
+    setCurrentStep(newArray);
     setIsXNext(!IsXNext);
+  }
+
+  function goNextBack(direction) {
+    const index = history.findIndex(value => value === currentStep);
+    const directionIndex = direction === 'next' ? index + 1 : index - 1;
+    if (directionIndex >= 0) {
+      const filteredStep = history[directionIndex];
+      if (filteredStep?.length) {
+        setValue(filteredStep);
+        setCurrentStep(filteredStep);
+      }
+    }
+  }
+
+  function reset() {
+    setValue(emptyArray);
+    setHistory([emptyArray]);
+    setCurrentStep([]);
   }
 
   function calculateWinner(board) {
@@ -49,7 +73,9 @@ export default function App() {
   let status;
   const winnerObj = calculateWinner(values);
   if (winnerObj) {
-    status = "Winner : " + winnerObj.winner;
+    status = 'Winner : ' + winnerObj.winner;
+  } else if (values.every(value => value !== null)) {
+    status = 'Draw!!!!'
   } else {
     status = 'Next turn : ' + (IsXNext ? 'X' : 'O');
   }
@@ -57,13 +83,18 @@ export default function App() {
   return (
     <div className='App'>
       <h1>Tic Tac Toe</h1>
-      <br /> <h4>{status}</h4> <br />
+      <h4>{status}</h4>
       <div className='board-row'>
         {
           values.map((value, index) =>
             <Square key={index} value={value} currentIndex={index} onClick={() => handleSquareClick(index)} winnerCombination={winnerObj?.combination} />
           )
         }
+        <div className='controls'>
+          <button className='button button-back' onClick={() => goNextBack('back')}>← Back</button>
+          <button className='button button-next' onClick={() => goNextBack('next')}>Next →</button>
+        </div>
+        <button className='button button-reset' onClick={reset}>Reset</button>
       </div>
     </div>
   );
